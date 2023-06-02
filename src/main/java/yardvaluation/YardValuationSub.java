6c9815d6c428c46c3a2b60fd8a12992c06c23b36
Ishanka Sandeepa <ishankasandeepa6@gmail.com>
2023-06-02 13:51:42 +0530

    Initialize Yard Valuation

--- src/main/java/yardvaluation/YardValuationSub.java
+++ src/main/java/yardvaluation/YardValuationSub.java
@@ -51,4 +51,66 @@ public WebElement clickTheYardValuation()
 	By var_clickTheYardValuation = By.xpath("//*[@id=\"/AnRkr/yard-management$Menu\"]/li[1]");
 	return driver.findElement(var_clickTheYardValuation);
 }
+public WebElement AddNewYardValuation()
+{
+	By var_AddNewYardValuation = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div/div/div[2]/div[1]/a/div");
+	return driver.findElement(var_AddNewYardValuation);
+}
+public WebElement EntertheVehicleNumberValuation()
+{
+	By var_EntertheVehicleNumberValuation = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[1]/div/div/div[1]/span/input");
+	return driver.findElement(var_EntertheVehicleNumberValuation);
+}
+//public WebElement EntertheContractNumberValuation()
+//{
+//	By var_EntertheContractNumberValuation = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[1]/div/div/div[1]/span/input");
+//	return driver.findElement(var_EntertheContractNumberValuation);
+//}
+public WebElement ClicktheSearchBtnValuation()
+{
+	By var_ClicktheSearchBtnValuation = By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[1]/div/div/div[3]/button");
+	return driver.findElement(var_ClicktheSearchBtnValuation);
+}
+public WebElement clicktheScrollBtnValuation()
+{
+	By var_clicktheScrollBtnValuation= By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[3]/div/div/div/div/div/div/div");
+	return driver.findElement(var_clicktheScrollBtnValuation);
+}
+public WebElement clicktheReValuationbtn()
+{
+	By var_clicktheReValuationbtn= By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/div[3]/div/div/div/div/div/div/div/table/tbody/tr[2]/td[7]/div/div/p/a/button/span");
+	return driver.findElement(var_clicktheReValuationbtn);
+}
+public WebElement EnterTheFSValue()
+{
+	By var_EnterTheFSValue= By.xpath("//*[@id=\"valuationFSV\"]");
+	return driver.findElement(var_EnterTheFSValue);
+}
+public WebElement ClickValuer()
+{
+	By var_ClickValuer= By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div/div/form[1]/div[2]/div[2]/div[1]/div[6]/div/div/div/div/div[1]/p/a");
+	return driver.findElement(var_ClickValuer);
+}
+public WebElement ClickSelectBtnValue()
+{
+	By var_ClickSelectBtnValue= By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div/div/div/div/div/div[2]/table/tbody/tr[2]/td[3]/span");
+	return driver.findElement(var_ClickSelectBtnValue);
+}
+public WebElement ScrollDownValuation()
+{
+	By var_ScrollDownValuation= By.xpath("//*[@id=\"root\"]/div/div/div[2]/div/div/div/div[2]/div/div/div/div/div[1]/div/div/form[1]/div[2]/div[2]");
+	return driver.findElement(var_ScrollDownValuation);
+}
+public WebElement FileUploadValue()
+{
+	By var_FileUploadValue= By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div/div/div/div/div/div[2]/table/tbody/tr[2]/td[3]/span");
+	return driver.findElement(var_FileUploadValue);
+}
+public WebElement ValuationConditionCheckBoxes()
+{
+	By var_ValuationConditionCheckBoxes= By.xpath("/html/body/div[3]/div/div[2]/div/div[2]/div[2]/div[2]/div/div/div/div/div/div[2]/table/tbody/tr[2]/td[3]/span");
+	return driver.findElement(var_ValuationConditionCheckBoxes);
+}
+
+
 }
--- src/test/java/stepDefinition/YardValuationStepDef.java
+++ src/test/java/stepDefinition/YardValuationStepDef.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 
 import com.beust.jcommander.internal.Lists;
 
-
+import clickOnAddNewYardIn.ClickOnAddNewYardInSub;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
@@ -20,7 +20,7 @@ public class YardValuationStepDef extends YardValuationMain {
 	@Given("I need to navigate Fusion X Loging page CANYV")
 	public void i_need_to_navigate_fusion_x_loging_page_canyv()  throws Throwable {
 		driver=inheritanceYV();
-		driver.get("https://qa-sl.fusionx.biz/web/home/AnRkr/dashboard");
+		driver.get("https://uat-sl.fusionx.biz/web/home/AnRkr/dashboard");
 		driver.manage().window().maximize();
 		Thread.sleep(2000); 
 	}
@@ -62,7 +62,7 @@ public class YardValuationStepDef extends YardValuationMain {
 	}
 	
 	@When("I click on Post Yard Management CANYV")
-	public void i_click_on_post_yard_management_canyv() {
+	public void i_click_on_post_yard_management_canyv() throws Throwable {
 		YardValuationSub var1=new YardValuationSub(driver);
 		List<String> browserTabs = Lists.newArrayList(driver.getWindowHandles());
 		driver.switchTo().window(browserTabs.get(1));
@@ -71,8 +71,139 @@ public class YardValuationStepDef extends YardValuationMain {
 	}
 	
 	@When("I click The Yard Valuation CANYV")
-	public void i_click_the_yard_valuation_canyv() {
+	public void i_click_the_yard_valuation_canyv() throws Throwable {
 		YardValuationSub var1=new YardValuationSub(driver);
 		var1.clickTheYardValuation().click();
+	}
+	
+	@When("I click The Add New Yard Valuation CANYV")
+	public void i_click_the_add_new_yard_valuation_canyv()throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1. AddNewYardValuation().click();
+	}
+	
+	@When("I  Enter the Vehicle Number CANYV")
+	public void i_enter_the_vehicle_number_canyv() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.EntertheVehicleNumberValuation().sendKeys("QB9335");
+		Thread.sleep(2000);
+	}
+//	@When("I Enter the Contract Number CANYV")
+//	public void i_enter_the_contract_number_canyv() throws Throwable {
+//		YardValuationSub var1=new YardValuationSub(driver);
+//		var1.EntertheContractNumberValuation().sendKeys("F7-02-3447DA");
+//		Thread.sleep(2000);
+//	}
+//	
+//	@When("I Enter the Chassis Number CANYV")
+//	public void i_enter_the_chassis_number_canyv() throws Throwable {
+//		YardValuationSub var1=new YardValuationSub(driver);
+//		var1.EntertheVehicleNumberValuation().sendKeys("KD0582");
+//		Thread.sleep(2000);
+//	}
+
+	@When("I Click  the Search btn CANYV")
+	public void i_click_the_search_btn_canyv()throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1. ClicktheSearchBtnValuation().click();
+	}
+	
+	@When("I Click  the Scroll btn CANYV")
+	public void i_click_the_scroll_btn_canyv() throws Throwable {
+		  Thread.sleep(2000);
+		  YardValuationSub  var1 = new YardValuationSub (driver);
+	      WebElement element = var1.clicktheScrollBtnValuation();
+	      JavascriptExecutor js = (JavascriptExecutor) driver;
+	      js.executeScript("arguments[0].scrollLeft = arguments[0].scrollWidth", var1.clicktheScrollBtnValuation());
+	}
+	
+	@When("I Click  the ReValuation btn CANYV")
+	public void i_click_the_re_valuation_btn_canyv() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.clicktheReValuationbtn().click();
+	}
+	
+	@When("I Enter  the FS Value  CANYV")
+	public void i_enter_the_fs_value_canyv() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.EnterTheFSValue().sendKeys("350");
+		Thread.sleep(3000);
+	}
+
+	@When("I Click Valuer  CANYV")
+	public void i_click_valuer_canyv() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.ClickValuer().click();
+		Thread.sleep(3000);
+	}
+	
+	@When("I Click  the Select Btn CANYV")
+	public void i_click_the_select_btn_canyv() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.ClickSelectBtnValue().click();
+		Thread.sleep(3000);
+	}
+	
+	@When("I  click the  Down Scroll Btn CANYV")
+	public void i_click_the_down_scroll_btn_canyv() throws Throwable {
+		Thread.sleep(2000);
+		YardValuationSub  var1 = new YardValuationSub (driver);
+	    WebElement element = var1.ScrollDownValuation();
+	    JavascriptExecutor js = (JavascriptExecutor) driver;
+	    js.executeScript("arguments[0].scrollLeft = arguments[0].scrollWidth", var1.ScrollDownValuation());
+	}
+	
+	@When("I Upload The File CANYV")
+	public void i_upload_the_file_canyv()throws Throwable {
+		Thread.sleep(3000);
+    	String filePath = "C:\\Users\\IshankaPu\\Pictures\\Screenshots";
+    	ClickOnAddNewYardInSub var1=new ClickOnAddNewYardInSub(driver);
+    	var1.FileUpload().sendKeys(filePath);
+	}
+	
+	@When("I Tick on Valuation Condition check boxes")
+	public void i_tick_on_valuation_condition_check_boxes()throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.ValuationConditionCheckBoxes().click();
+		Thread.sleep(3000);
+	}
+	
+	@When("I Click on Add New")
+	public void i_click_on_add_new()throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.ClickSelectBtnValue().click();
+		Thread.sleep(3000);
+	}
+	
+	@When("I Enter External Valuation")
+	public void i_enter_external_valuation() throws Throwable{
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.EnterTheFSValue().sendKeys("350");
+		Thread.sleep(3000);
+	}
+	
+	@When("I Click on Save button")
+	public void i_click_on_save_button() throws Throwable {
+		YardValuationSub var1=new YardValuationSub(driver);
+		var1.ClickSelectBtnValue().click();
+		Thread.sleep(3000);
+	}
+	
+	@When("I View uploaded report")
+	public void i_view_uploaded_report() throws Throwable{
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
-}
+
+
+
+
+
+
+	
+	  
+
+
+
+
