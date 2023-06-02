21b09ea0e7948d91b8f06585b9443ea6e03a0a2b
Nipun Thiwanka <iit18050@std.uwu.ac.lk>
2023-06-02 11:31:58 +0530

    Import Selenium by Maven Repo

---
+++ src/test/java/runner/ResellingPriceUpdateRunner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/feature",
+		glue = "stepDefinition" , tags="@click_on_Reselling_Price_Update")
+public class ResellingPriceUpdateRunner {
+
+}
